fd5fad9e667b605691887ab783190e6bc4c72530
Sumin <Sumin@DESKTOP-O7UD9KM>
2021-07-26 14:42:48 +0900

    자원서버

---
+++ portfolioauthserver/src/main/java/jj/group/portfolio/PortfolioauthserverApplication.java
@@ -0,0 +1,13 @@
+package jj.group.portfolio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PortfolioauthserverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PortfolioauthserverApplication.class, args);
+	}
+
+}
---
+++ portfolioauthserver/src/test/java/jj/group/portfolio/PortfolioauthserverApplicationTests.java
@@ -0,0 +1,13 @@
+package jj.group.portfolio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PortfolioauthserverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
