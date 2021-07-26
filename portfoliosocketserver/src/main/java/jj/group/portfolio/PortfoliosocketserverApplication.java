b5c4ea5cad5440791269ce5054e5f95aabab1d1c
Sumin <Sumin@DESKTOP-O7UD9KM>
2021-07-26 14:43:10 +0900

    소켓 서버

---
+++ portfoliosocketserver/src/main/java/jj/group/portfolio/PortfoliosocketserverApplication.java
@@ -0,0 +1,13 @@
+package jj.group.portfolio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PortfoliosocketserverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PortfoliosocketserverApplication.class, args);
+	}
+
+}
---
+++ portfoliosocketserver/src/test/java/jj/group/portfolio/PortfoliosocketserverApplicationTests.java
@@ -0,0 +1,13 @@
+package jj.group.portfolio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PortfoliosocketserverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
