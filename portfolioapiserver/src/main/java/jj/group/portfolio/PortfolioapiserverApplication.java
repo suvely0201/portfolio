2ec77934e0711c9d9996f0818f794ef82a3848c9
Sumin <Sumin@DESKTOP-O7UD9KM>
2021-07-26 14:41:35 +0900

    인증 서버

---
+++ portfolioapiserver/src/main/java/jj/group/portfolio/PortfolioapiserverApplication.java
@@ -0,0 +1,13 @@
+package jj.group.portfolio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PortfolioapiserverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PortfolioapiserverApplication.class, args);
+	}
+
+}
---
+++ portfolioapiserver/src/test/java/jj/group/portfolio/PortfolioapiserverApplicationTests.java
@@ -0,0 +1,13 @@
+package jj.group.portfolio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PortfolioapiserverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
